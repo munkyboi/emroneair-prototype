17f52a9bda6c540d128be40143799ff7bdf34cd2
munky <munky.work@yahoo.com.sg>
2020-10-27 02:17:10 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -10,24 +10,41 @@ const imagemin = require('gulp-imagemin');
 const concat = require('gulp-concat');
 const jsImport = require('gulp-js-import');
 const sourcemaps = require('gulp-sourcemaps');
-const htmlPartial = require('gulp-html-partial');
+// const htmlPartial = require('gulp-html-partial');
+const ejs = require('gulp-ejs')
+const rename = require('gulp-rename')
 const clean = require('gulp-clean');
 const isProd = process.env.NODE_ENV === 'prod';
 
-const htmlFile = [
-    'src/pages/*.html'
-]
+// const htmlFile = [
+//     'src/pages/*.html'
+// ]
 
 function html() {
-    return gulp.src(htmlFile)
-        .pipe(htmlPartial({
-            basePath: 'src/pages/partials/'
-        }))
+    return gulp.src("src/pages/**/*.ejs")
+        .pipe(ejs({ title: 'gulp-ejs' }))
+        .pipe(rename({ extname: '.html' }))
         .pipe(gulpIf(isProd, htmlmin({
             collapseWhitespace: true
         })))
         .pipe(gulp.dest('dist'));
 }
+
+// function htmlejs() {
+//     return gulp.src("src/templates/**/*.ejs")
+//         .pipe(ejs({}))
+// }
+
+// function html() {
+//     return gulp.src(htmlFile)
+//         .pipe(htmlPartial({
+//             basePath: 'src/pages/partials/'
+//         }))
+//         .pipe(gulpIf(isProd, htmlmin({
+//             collapseWhitespace: true
+//         })))
+//         .pipe(gulp.dest('dist'));
+// }
 
 function css() {
     return gulp.src('src/sass/style.scss')
@@ -111,7 +128,8 @@ function browserSyncReload(done) {
 
 
 function watchFiles() {
-    gulp.watch('src/**/*.html', gulp.series(html, browserSyncReload));
+    gulp.watch('src/**/*.ejs', gulp.series(html, browserSyncReload));
+    // gulp.watch('src/**/*.html', gulp.series(html, browserSyncReload));
     gulp.watch('src/**/*.scss', gulp.series(css, browserSyncReload));
     gulp.watch('src/**/*.js', gulp.series(js, browserSyncReload));
     gulp.watch('src/data/**/*.json', gulp.series(json, browserSyncReload));
@@ -129,6 +147,7 @@ function del() {
 
 exports.css = css;
 exports.html = html;
+// exports.htmlejs = htmlejs;
 exports.js = js;
 exports.jsmaps = jsmaps;
 exports.jsdata = jsdata;
--- src/js/script.js
+++ src/js/script.js
@@ -9,6 +9,11 @@
 // ============================================================
 // ============================================================
 
+
+
+let __PAGE__ = ''
+let __VH__ = window.innerHeight * 0.01;
+
 (() => {
   let quickviewProdId = 0
   const toastDelay = 5000
@@ -21,12 +26,30 @@
 
   // INITIALIZE SCRIPTS
   const initScripts = () => {
-    $(window).scroll()
-    $(window).resize()
+    $(window).trigger('scroll')
+    $(window).trigger('resize')
 
-    $(window).scroll(function(e) {
-      const scrolled = document.body.scrollTop || document.documentElement.scrollTop
-      if (scrolled >= 60) {
+    document.documentElement.style.setProperty('--vh', `${__VH__}px`);
+
+    // $(window).scroll(function(e) {
+    //   const scrolled = document.body.scrollTop || document.documentElement.scrollTop
+    //   if (scrolled >= 60) {
+    //     $('body').addClass('page-scrolled')
+    //   } else {
+    //     $('body').removeClass('page-scrolled')
+    //   }
+    // })
+
+    // MAIN
+    const MainContainer = document.querySelector('body > app > .main > .content > .context')
+    const MainPS = new PerfectScrollbar(MainContainer, {
+      wheelSpeed: 2,
+      wheelPropagation: false,
+      minScrollbarLength: 20,
+      suppressScrollX: true
+    })
+    MainContainer.addEventListener('ps-scroll-y', (e) => {
+      if (e.target.scrollTop > 60) {
         $('body').addClass('page-scrolled')
       } else {
         $('body').removeClass('page-scrolled')
@@ -35,9 +58,11 @@
 
     // SIDEBAR
     $('.burger').on('click', function(e) {
+      e.preventDefault()
       $('body').toggleClass('show-sidebar')
     })
     $('.sidebar-overlay').on('click', function(e) {
+      e.preventDefault()
       $('body').toggleClass('show-sidebar')
     })
 
@@ -52,10 +77,49 @@
 
     // ASIDE LISTS
     $('.aside .list-link').on('click', function(e) {
+      e.preventDefault()
       $('body').addClass('show-context')
     })
     $('.control-hide').on('click', function(e) {
+      e.preventDefault()
       $('body').removeClass('show-context')
+      setTimeout(() => {
+        MainContainer.scrollTo(0,0)
+      }, 300)
+    })
+    const AsideContainer = document.querySelector('.aside  .list')
+    const AsidePS = new PerfectScrollbar(AsideContainer, {
+      wheelSpeed: 2,
+      wheelPropagation: false,
+      minScrollbarLength: 20
+    })
+
+    // ASIDE SEARCH FIELD
+    $('.aside .search .form-control').on('keyup', function(e) {
+      if (e.currentTarget.value.length > 0) {
+        $(this).parent().addClass('not-empty')
+      } else {
+        $(this).parent().removeClass('not-empty')
+      }
+    })
+    $('.aside .search .clear').on('click', function(e) {
+      e.preventDefault()
+      $(this).parent().removeClass('not-empty')
+      $(this).parent().find('.form-control').val('')
+    })
+
+    // TABS SCROLLBAR
+    const TabsContainer = document.querySelector('.tabs .tabs-container')
+    const TabsPS = new PerfectScrollbar(TabsContainer, {
+      // handlers: ['keyboard', 'wheel', 'touch'],
+      wheelSpeed: 2,
+      wheelPropagation: false,
+      minScrollbarLength: 20
     })
   }
 })()
+
+$(window).on('resize', function(e) {
+  __VH__ = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${__VH__}px`);
+})
